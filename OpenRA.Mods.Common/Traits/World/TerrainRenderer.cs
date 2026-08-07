b643a93014f5b627d4ea451ec2c2c33d37ce7e7f
DRB HOME LAPTOP <drb_laptop@DRBs-MacBook-Pro.local>
2026-08-07 11:32:13 -0400

    Diag: log terrain palette/HasColorShift/channel for one Lunar cell

--- OpenRA.Mods.Common/Traits/World/TerrainRenderer.cs
+++ OpenRA.Mods.Common/Traits/World/TerrainRenderer.cs
@@ -96,6 +96,8 @@ namespace OpenRA.Mods.Common.Traits
 			map.Height.CellEntryChanged += UpdateCell;
 		}
 
+		static bool diagPrinted;
+
 		public void UpdateCell(CPos cell)
 		{
 			var tile = map.Tiles[cell];
@@ -105,6 +107,13 @@ namespace OpenRA.Mods.Common.Traits
 
 			var sprite = tileCache.TileSprite(tile);
 			var paletteReference = worldRenderer.Palette(palette);
+
+			if (!diagPrinted && terrainInfo.Id == "LUNAR")
+			{
+				diagPrinted = true;
+				Console.WriteLine($"[diag] terrain cell {cell}: tile.Type={tile.Type} palette-name={palette} sprite.Channel={sprite.Channel} HasColorShift={paletteReference.HasColorShift} TextureIndex={paletteReference.TextureIndex}");
+			}
+
 			spriteLayer.Update(cell, sprite, paletteReference);
 		}
 
